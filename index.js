e9a8f2045cc7892a52226164793e0653183e42ba
codalo <ckodalo@gmail.com>
2022-08-05 03:02:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+const arr = [];
+function writeCards(arr, event) {
+    let newarr = [];
+    for (let i = 0; i < arr.length; i++) {
+        newarr.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+
+    }
+return newarr;
+}
+
+function countDown(number) {
+while (number > 0) {
+     console.log(number);
+    number--;
+    }
+console.log(number);
+}
